3c30ca8c36c696a12ff1d43391e92dc28c9c86c1
andrey.donenko <andrey.donenko@jetruby.com>
2021-03-29 18:59:51 +0300

    some changes

---
+++ app/services/broadcast_message_service.rb
@@ -0,0 +1,17 @@
+
+class BroadcastMessageService
+    attr_reader :user, :data
+    def initialize(args)
+      @user = args[:user]
+      @data = args[:data]
+    end
+  
+    def perform
+      message = user.messages.create text: data['message']
+  
+      ActionCable.server.broadcast 'room_channel',
+                                   message: MessageSerializer.new(message).to_h
+  
+      SendNotificationJob.perform_later message.id
+    end
+  end
---
+++ app/services/mock_message.rb
@@ -0,0 +1,11 @@
+
+class MockMessage
+    def perform
+      current_user = User.all.sample
+      text = Faker::LeagueOfLegends.quote
+      message = current_user.messages.create text: text
+      ActionCable.server.broadcast "room_channel", message: MessageSerializer.new(message).to_h
+  
+      SendNotificationJob.perform_later message.id
+    end
+  end
